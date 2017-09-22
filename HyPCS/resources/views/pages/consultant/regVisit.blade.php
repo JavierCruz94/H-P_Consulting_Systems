7b9626219abf3519823036b29dfa85df0ae371a7
Donato <donato_ath@hotmail.com>
2017-09-21 22:35:20 -0500

    Css Registrar Visita

--- HyPCS/resources/views/pages/consultant/regVisit.blade.php
+++ HyPCS/resources/views/pages/consultant/regVisit.blade.php
@@ -9,49 +9,56 @@
 
 @section('content')
     <br> <br> <br>
-    <h3><span class="label label-default" id="firstAdd">Registrar Visita</span></h3>
-    <h5 class="adminMarginAdd">Cliente: </h5>
-    <div class="input-group">
-        <input type="text" class="form-control" placeholder="Nombre del Cliente" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
+    <div>
+        <div style=" width: 25%; height: 100%; float: left;">
+            <h3><span class="label label-default" id="firstAdd">Registrar Visita</span></h3>
+            <h5 class="adminMarginAdd">Cliente: </h5>
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Nombre del Cliente" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
+            </div>
+            <button type="button" class="btn btn-default" style="margin-top: 1em">Buscar solicitudes</button></td>
+            <div>
+                <h5 class="adminMarginAdd">Comentarios extra:</h5>
+                <div class="input-group">
+                    <textarea class="form-control" placeholder="Comentarios" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em; height: 150px; width: 250px"> </textarea>
+                </div>
+                <h5 class="adminMarginAdd">Hora llegada:</h5>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="HH:MM" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
+                </div>
+                <h5 class="adminMarginAdd">Hora salida:</h5>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="HH:MM" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
+                </div>
+                <button type="button" class="btn btn-default" style="margin-top: 1em">Generar reporte</button></td>
+            </div>
+        </div>
+        <div style = "width: 50%; height: 100%; float: left;">
+            <h5 class="adminMarginAdd">Solicitudes</h5>
+            <div class="table">
+                <table class="table">
+                    <tr style="font-weight: bold; margin: auto">
+                        <td> Marcar </td>
+                        <td> Asunto </td>
+                    </tr>
+                    <tr>
+                        <td><div class="checkbox">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div></td>
+                        <td>AAAAAA</td>
+                    </tr>
+                    <tr>
+                        <td><div class="checkbox">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div></td>
+                        <td>CCCCC</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
-    <button type="button" class="btn btn-default" style="margin-top: 1em">Buscar solicitudes</button></td>
-
-    <h5 class="adminMarginAdd">Solicitudes</h5>
-    <div class="table">
-        <table class="table">
-            <tr style="font-weight: bold; margin: auto">
-                <td> Marcar </td>
-                <td> Asunto </td>
-            </tr>
-            <tr>
-                <td><div class="checkbox">
-                        <label>
-                            <input type="checkbox">
-                        </label>
-                    </div></td>
-                <td>AAAAAA</td>
-            </tr>
-            <tr>
-                <td><div class="checkbox">
-                        <label>
-                            <input type="checkbox">
-                        </label>
-                    </div></td>
-                <td>CCCCC</td>
-            </tr>
-        </table>
-    </div>
-    <h5 class="adminMarginAdd">Comentarios extra:</h5>
-    <div class="input-group">
-        <input type="text" class="form-control" placeholder="Comentarios" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
-    </div>
-    <h5 class="adminMarginAdd">Hora llegada:</h5>
-    <div class="input-group">
-        <input type="text" class="form-control" placeholder="Comentarios" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
-    </div>
-    <h5 class="adminMarginAdd">Hora salida:</h5>
-    <div class="input-group">
-        <input type="text" class="form-control" placeholder="Comentarios" aria-describedby="basic-addon2" style=" margin-top: -.5em; margin-left: 1em;">
-    </div>
-    <button type="button" class="btn btn-default" style="margin-top: 1em">Generar reporte</button></td>
 @endsection
