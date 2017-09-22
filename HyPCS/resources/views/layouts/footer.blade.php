18f6086d5c37e8897ab47f36c3232059a462ff09
Elí Emmanuel Linares Romero <eli.emmanuel01@gmail.com>
2017-09-21 20:24:36 -0500

    footer listo

--- HyPCS/resources/views/layouts/footer.blade.php
+++ HyPCS/resources/views/layouts/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="footer">
     <div class="container">
-        <span class="navbar navbar-fixed-bottom" style="padding-left: 2em; padding-top: 1em; background-color: lightgrey">
-            hectorcg@hypcs.com
-        </span>
+        <nav class="navbar navbar-fixed-bottom" style="padding-left: 2em; padding-top: 1em; background-color: lightgrey">
+            <div style="padding-right: 2em;float: right;">
+                hectorcg@hypcs.com
+            </div>
+        </nav>
     </div>
 </footer>
