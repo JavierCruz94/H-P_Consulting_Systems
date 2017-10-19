a6f30f21dd45ae464dad843136a4fe221a622ac3
JavierCruz94 <javiercruz_94@hotmail.com>
2017-10-19 00:42:56 -0500

    Login start

--- HyPCS/app/User.php
+++ HyPCS/app/User.php
@@ -30,4 +30,37 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAdmin()
+    {
+        return $this->role == 'admin';
+    }
+
+    public function isConsultant()
+    {
+        return $this->role == 'consultant';
+    }
+
+    public function isCustomer()
+    {
+        return $this->role == 'customer';
+    }
+
+    public function userInfo()
+    {
+        if ($this->role == 'admin') {
+            return $this->hasOne('App\Admin');
+        }
+
+        if ($this->role == 'consultant') {
+            return $this->hasOne('App\Consultant');
+        }
+
+        if ($this->role == 'customer') {
+            return $this->hasOne('App\Customer');
+        }
+
+        return false;
+    }
+
 }
